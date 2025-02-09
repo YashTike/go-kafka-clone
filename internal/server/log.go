29e361b4e1d1e1ad2e1fef1340078781c588e0d7
YashTike <yashtike.usa@gmail.com>
2025-02-09 00:47:45 -0800

    Created basic HTTP/JSON server for listening and handling read and write requests to Commit Log

--- internal/server/log.go
+++ internal/server/log.go
@@ -13,28 +13,27 @@ type Record struct {
 }
 
 type Log struct {
-	records []Record
 	mu sync.Mutex
+	records []Record
 }
 
 func NewLog() *Log {
 	return &Log{}
 }
 
-func (c *Log) Append(record Record) (uint64, error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	record.Offset = uint64(len(c.records))
-	c.records = append(c.records, record)
+func (l *Log) Append(record Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	record.Offset = uint64(len(l.records))
+	l.records = append(l.records, record)
 	return record.Offset, nil
 }
 
-func (c *Log) Read(offset uint64) (Record, error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	if offset >= uint64(len(c.records)) {
+func (l *Log) Read(offset uint64) (Record, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	if offset >= uint64(len(l.records)) {
 		return Record{}, ErrOffsetNotFound
 	}
-	return c.records[offset], nil
+	return l.records[offset], nil
 }
-
